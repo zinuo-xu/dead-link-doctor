ea8d197a96d4321c3faee3117b8f9578d82a7866
xuzinuo <2426503000@qq.com>
2026-06-03 23:11:11 +0800

    fix: correct off-by-one error in iteration

--- internal/checker/retry.go
+++ internal/checker/retry.go
@@ -1,3 +1,5 @@
 # dead-link-doctor - chore: update dependencies to latest versions
 
 # docs: improve installation instructions (incremental change 2)
+
+# fix: correct off-by-one error in iteration (incremental change 3)
